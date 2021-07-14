3a20375aba00ef728cea8c65fb4a51e649029e2b
eliastellati <eliastellatibvb@gmail.com>
2021-07-14 15:36:44 +0200

    header rough

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,32 @@
+<header>
+   <div class="container top">
+        <span class="uppercase">Dc power<i class="fas fa-trademark"></i> visa</span>
+        <i class="far fa-registered"></i>
+        <span class="uppercase">Additional dc sites</span>
+        <i class="fas fa-caret-down"></i>  
+   </div>
+   <div class="container nav-bar">
+        <img src="{{ asset("images/dc-logo.png") }}" alt="DC Logo">
+        <ul>
+            <li class="uppercase">Characters</li>
+            <li class="uppercase">Comics</li>
+            <li class="uppercase">Movies</li>
+            <li class="uppercase">Tv</li>
+            <li class="uppercase">Games</li>
+            <li class="uppercase">Collectibles</li>
+            <li class="uppercase">Videos</li>
+            <li class="uppercase">Fans</li>
+            <li class="uppercase">News</li>
+            <li class="uppercase">Shop</li>
+            <i class="fas fa-caret-down"></i>
+        </ul>
+        <div id="search">
+            <input type="text" placeholder="Search">
+            <i class="fas fa-search"></i>
+        </div>
+   </div>
+   <div class="jumbotron">
+       
+   </div>
+
+</header>
